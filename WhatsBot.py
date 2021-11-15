4c67e75d0bb19a725c410e18f47abd4983bc679b
Sanjev Kumar T <sanjevkumarbscit2019@gmail.com>
2021-11-15 22:03:30 +0530

    Update WhatsBot.py

--- WhatsBot.py
+++ WhatsBot.py
@@ -2,7 +2,6 @@ import random
 from time import time,ctime,sleep
 
 from selenium import webdriver
-from selenium_stealth import stealth
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
